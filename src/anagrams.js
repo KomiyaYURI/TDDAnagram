b1e8d32b1ad9fc86db03333d31661f88caa23836
KomiyaYURI <879513168@qq.com>
2018-09-09 23:34:13 +0800

    TDD anagrams

---
+++ src/anagrams.js
@@ -0,0 +1,21 @@
+const  = (string) => {
+    if(string.length == 0) {
+        return new Set()
+    }
+    if (string.length == 1) {
+        return new Set(string)
+    }
+    let ret = new Set();
+    string.split('').map((item, index, array) => {
+        let subStringArray = [...array];
+        subStringArray.splice(subStringArray.indexOf(item), 1);
+        [...anagram(subStringArray.join(''))].map(subString=> {
+            ret.add(`${item}${subString}`);
+        })
+    })
+
+    return ret;
+}
+
+
+module.exports = anagram
---
+++ test/anagrams.test.js
@@ -0,0 +1,36 @@
+const anagram = require("../src/anagrams");
+
+
+test('should return empty array when inputing empty string', () => {
+    expect(anagram('')).toEqual(new Set());
+});
+
+test('should return [a] when inputing a', () => {
+    expect(anagram('a')).toEqual(new Set('a'));
+});
+
+test('should return [ab, ba] when inputing ab', () => {
+    let expectedSet = new Set()
+    expectedSet.add('ab')
+    expectedSet.add('ba')
+    expect(anagram('ab')).toEqual(expectedSet);
+});
+
+
+test('should return a lot when inputing abc', () => {
+    expect([...anagram('abc')].length).toEqual(6);
+});
+
+test('should return specific set when inputing abc', () => {
+    let expectedSet = new Set()
+    expectedSet.add('abc')
+    expectedSet.add('acb')
+    expectedSet.add('bac')
+    expectedSet.add('bca')
+    expectedSet.add('cab')
+    expectedSet.add('cba')
+    expect(anagram('abc')).toEqual(expectedSet);
+});
+test('should return a lot when inputing abcd', () => {
+    expect([...anagram('abcd')].length).toEqual(24);
+});
